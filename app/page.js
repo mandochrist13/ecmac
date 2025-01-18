7633dbcb5d944b9bab2c9143c83ee93071f7bc1a
mando_christ <mandochrist13@gmail.com>
2025-01-18 03:05:20 +0100

    section filière et promesse

--- app/page.js
+++ app/page.js
@@ -1,10 +1,13 @@
-import Hero from "./components/hero";
+import Promise from "./components/promise";
 import HeroCarousel from "./components/HeroCarousel";
+import Filiere from "./components/filiere";
 
 export default function Home() {
   return (
     <div className="">
      <HeroCarousel />
+     <Promise />
+     <Filiere />
     </div>
   );
 }
